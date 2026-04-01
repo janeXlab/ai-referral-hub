14aa6ce21e2ef947a243e597023119d54b27008e
娟娟 <598974663@qq.com>
2026-04-01 21:36:28 +0800

    完成了 Vercel Analytics 和 Speed Insights 的集成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = GeistSans;
